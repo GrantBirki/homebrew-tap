c424a29fac9d3c0248a08601b806c37d61e5ab31
GrantBirki <grant.birkinbine@gmail.com>
2025-01-17 11:24:41 -0800

    add crystal 1.14.1

---
+++ Formula/crystal.rb
@@ -0,0 +1,171 @@
+class Crystal < Formula
+  desc "Fast and statically typed, compiled language with Ruby-like syntax"
+  homepage "https://crystal-lang.org/"
+  license "Apache-2.0"
+
+  stable do
+    url "https://github.com/crystal-lang/crystal/archive/refs/tags/1.14.1.tar.gz"
+    sha256 "3e0e30f6c57bd86dbd52ccde4e42fc57c178f6bef47edc96b8c911b3fb12bd5b"
+
+    resource "shards" do
+      url "https://github.com/crystal-lang/shards/archive/refs/tags/v0.19.0.tar.gz"
+      sha256 "25204d48ae05e4ad389cea9c34405725f861149b0679b43aa2d9fc8a961a5480"
+    end
+  end
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any,
+           arm64_sequoia: "dd1c819aae4412d8a3bed08d9ef49946b7163dc36eaf95d4c0dc6c7ef0c48dd3"
+    sha256 cellar: :any,
+           arm64_sonoma: "6e3636f2a0696af1c8cef9f6c7ec2588986e6ff5b32cf689a2972ed3f72e84f0"
+    sha256 cellar: :any,
+           arm64_ventura: "2346e6eca00240b7754732b26857d74e1dd28bdd9525b333e9c54828aef006a2"
+    sha256 cellar: :any,
+           sonoma: "3583abf05711c554e2e836595a3392b7491a44dc5f1e7094de7cf52c89c861e3"
+    sha256 cellar: :any,
+           ventura: "d1c433c49d3c9579fb7744bf631ceffa7a1776dc8b1112c6a08330f0b8927841"
+    sha256 cellar: :any_skip_relocation,
+           x86_64_linux: "61af0672fb356bfc6d9baf6ad3ad4ab9dadeb01c3ed57c3283c985d0ff2ef7c3"
+  end
+
+  head do
+    url "https://github.com/crystal-lang/crystal.git", branch: "master"
+
+    resource "shards" do
+      url "https://github.com/crystal-lang/shards.git", branch: "master"
+    end
+  end
+
+  depends_on "bdw-gc"
+  depends_on "gmp" # std uses it but it's not linked
+  depends_on "libevent"
+  depends_on "libyaml"
+  depends_on "llvm"
+  depends_on "openssl@3" # std uses it but it's not linked
+  depends_on "pcre2"
+  depends_on "pkgconf" # @[Link] will use pkg-config if available
+
+  uses_from_macos "libffi" # for the interpreter
+
+  # It used to be the case that every new crystal release was built from a
+  # previous release, except patches. Crystal is updating its policy to
+  # allow 4 minor releases of compatibility unless otherwise specified.
+  # Therefore, the boot version should have the MINOR component be
+  # between the current minor - 4 and current minor - 1.
+  #
+  # See: https://github.com/Homebrew/homebrew-core/pull/81318
+  resource "boot" do
+    boot_version = Version.new("1.10.1-1")
+    version boot_version
+
+    on_macos do
+      url "https://github.com/crystal-lang/crystal/releases/download/#{boot_version.major_minor_patch}/crystal-#{boot_version}-darwin-universal.tar.gz"
+      # version boot_version
+      sha256 "e6490e6d09745483bacea43c4d8974273632526c1f98f13db5aae0a5fc2c7924"
+    end
+
+    on_linux do
+      on_intel do
+        url "https://github.com/crystal-lang/crystal/releases/download/#{boot_version.major_minor_patch}/crystal-#{boot_version}-linux-x86_64.tar.gz"
+        # version boot_version
+        sha256 "1742e3755d3653d1ba07c0291f10a517fa392af87130dba4497ed9d82c12348b"
+      end
+    end
+  end
+
+  # Check version in `shard.lock` in shards repo.
+  resource "molinillo" do
+    url "https://github.com/crystal-lang/crystal-molinillo/archive/refs/tags/v0.2.0.tar.gz"
+    sha256 "e231cf2411a6a11a1538983c7fb52b19e650acc3338bd3cdf6fdb13d6463861a"
+  end
+
+  def install
+    llvm = deps.find { |dep| dep.name.match?(/^llvm(@\d+)?$/) }
+               .to_formula
+    non_keg_only_runtime_deps = deps.filter_map { |dep| dep.to_formula unless dep.build? }
+                                    .reject(&:keg_only?)
+
+    resource("boot").stage "boot"
+    ENV.append_path "PATH", "boot/bin"
+    ENV["LLVM_CONFIG"] = llvm.opt_bin / "llvm-config"
+    ENV["CRYSTAL_LIBRARY_PATH"] = ENV["HOMEBREW_LIBRARY_PATHS"]
+    ENV.append_path "CRYSTAL_LIBRARY_PATH", MacOS.sdk_path_if_needed / "usr/lib" if OS.mac? && MacOS.sdk_path_if_needed
+    non_keg_only_runtime_deps.each do |dep|
+      # Our just built `crystal` won't link with some dependents (e.g. `bdw-gc`, `libevent`)
+      # unless they're explicitly added to `CRYSTAL_LIBRARY_PATH`. The keg-only dependencies
+      # are already in `HOMEBREW_LIBRARY_PATHS`, so there is no need to add them.
+      ENV.prepend_path "CRYSTAL_LIBRARY_PATH", dep.opt_lib
+    end
+
+    crystal_install_dir = OS.linux? ? libexec : bin
+    stdlib_install_dir = pkgshare
+
+    # Avoid embedding HOMEBREW_PREFIX references in `crystal` binary.
+    config_library_path = "\\$$ORIGIN/#{HOMEBREW_PREFIX.relative_path_from(crystal_install_dir)}/lib"
+    config_path = "\\$$ORIGIN/#{stdlib_install_dir.relative_path_from(crystal_install_dir)}/src"
+
+    release_flags = ["release=true", "FLAGS=--no-debug"]
+    crystal_build_opts = release_flags + [
+      "CRYSTAL_CONFIG_LIBRARY_PATH=#{config_library_path}",
+      "CRYSTAL_CONFIG_PATH=#{config_path}",
+      "interpreter=true"
+    ]
+    crystal_build_opts << "CRYSTAL_CONFIG_BUILD_COMMIT=#{Utils.git_short_head}" if build.head?
+
+    # Build crystal
+    (buildpath / ".build").mkpath
+    system "make", "deps"
+    system "make", "crystal", *crystal_build_opts
+
+    # Build shards (with recently built crystal)
+    resource("shards").stage do
+      require "yaml"
+
+      shard_lock = YAML.load_file("shard.lock")
+      required_molinillo_version = shard_lock.dig("shards", "molinillo", "version")
+      available_molinillo_version = resource("molinillo").version.to_s
+      odie "`molinillo` resource is outdated!" if required_molinillo_version != available_molinillo_version
+
+      resource("molinillo").stage "lib/molinillo"
+
+      shards_build_opts = release_flags + [
+        "CRYSTAL=#{buildpath}/bin/crystal",
+        "SHARDS=false"
+      ]
+      shards_build_opts << "SHARDS_CONFIG_BUILD_COMMIT=#{Utils.git_short_head}" if build.head?
+      system "make", "bin/shards", *shards_build_opts
+
+      # Install shards
+      bin.install "bin/shards"
+      man1.install "man/shards.1"
+      man5.install "man/shard.yml.5"
+    end
+
+    # Install crystal
+    crystal_install_dir.install ".build/crystal"
+    stdlib_install_dir.install "src"
+
+    bash_completion.install "etc/completion.bash" => "crystal"
+    zsh_completion.install "etc/completion.zsh" => "_crystal"
+    fish_completion.install "etc/completion.fish" => "crystal.fish"
+
+    man1.install "man/crystal.1"
+
+    return unless OS.linux?
+
+    # Wrapper script so that Crystal can find libraries in HOMEBREW_PREFIX
+    (bin / "crystal").write_env_script(
+      crystal_install_dir / "crystal",
+      LD_RUN_PATH: "${LD_RUN_PATH:+${LD_RUN_PATH}:}#{HOMEBREW_PREFIX}/lib"
+    )
+  end
+
+  test do
+    assert_match "1", shell_output("#{bin}/crystal eval puts 1")
+  end
+end
