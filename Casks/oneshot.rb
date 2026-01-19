67825893e31a86cad6231a032784d06607a3c6f8
GrantBirki <grant.birkinbine@gmail.com>
2026-01-18 22:54:02 -0800

    Add OneShot cask for macOS screenshot utility

---
+++ Casks/oneshot.rb
@@ -0,0 +1,11 @@
+cask "oneshot" do
+  version "0.0.2"
+  sha256 "551ec4ce37d145788abf105d8a0f7564983ebe98d792d490318387afdfa83771"
+
+  url "https://github.com/grantbirki/oneshot/releases/download/v#{version}/OneShot.zip"
+  name "OneShot"
+  desc "Open source screenshot utility for macOS"
+  homepage "https://github.com/grantbirki/oneshot"
+
+  app "OneShot.app"
+end
