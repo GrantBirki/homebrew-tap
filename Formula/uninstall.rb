4d2623cb7c7eadaa7ef9ad26edbb4f438434c2c4
GrantBirki <grant.birkinbine@gmail.com>
2026-01-25 21:51:58 -0800

    Fix version declaration order in uninstall formula

--- Formula/uninstall.rb
+++ Formula/uninstall.rb
@@ -1,10 +1,10 @@
 class Uninstall < Formula
   desc "CLI tool for macOS to uninstall an app from your system"
   homepage "https://github.com/GrantBirki/uninstall"
+  version "1.0.0"
   url "https://github.com/GrantBirki/uninstall/releases/download/v#{version}/uninstall-#{version}.tar.gz"
   sha256 "6ac087a96c6cfc085c4a2e0257a57f3c9a1e5acb4666b69c8e448a462ece4486"
   license "MIT"
-  version "1.0.0"
 
   livecheck do
     url :url
