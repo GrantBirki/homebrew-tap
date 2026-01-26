b3776705cbeafc46ae85982273f7aace2fb1be8e
GrantBirki <grant.birkinbine@gmail.com>
2026-01-25 21:45:37 -0800

    Update uninstall formula: change URL to release tarball and versioning

--- Formula/uninstall.rb
+++ Formula/uninstall.rb
@@ -1,8 +1,15 @@
 class Uninstall < Formula
   desc "CLI tool for macOS to uninstall an app from your system"
   homepage "https://github.com/GrantBirki/uninstall"
-  url "https://github.com/GrantBirki/uninstall.git", :branch => 'main'
-  version "1.0"
+  url "https://github.com/GrantBirki/uninstall/releases/download/v#{version}/uninstall-#{version}.tar.gz"
+  sha256 "6ac087a96c6cfc085c4a2e0257a57f3c9a1e5acb4666b69c8e448a462ece4486"
+  license "MIT"
+  version "1.0.0"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   def install
     bin.install "uninstall"
