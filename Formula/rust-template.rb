f29fafd2a1536bad1158c3109a9f1cd6baf6416c
GrantBirki <grant.birkinbine@gmail.com>
2026-01-28 20:35:59 -0800

    Update rust-template formula: bump version to 0.1.6 and update SHA256 checksums

--- Formula/rust-template.rb
+++ Formula/rust-template.rb
@@ -1,22 +1,22 @@
 class RustTemplate < Formula
   desc "Minimal hello-world Rust CLI template"
   homepage "https://github.com/GrantBirki/rust-template"
-  version "0.1.0"
+  version "0.1.6"
   license "MIT"
 
   on_macos do
     url "https://github.com/GrantBirki/rust-template/releases/download/v#{version}/rust-template_v#{version}_darwin-universal.tar.gz"
-    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    sha256 "bb0ee285fcbdabbf6f483f39031a18e7b46d62017d4b360570b7e92cc70b86df"
   end
 
   on_linux do
     on_intel do
       url "https://github.com/GrantBirki/rust-template/releases/download/v#{version}/rust-template_v#{version}_linux-amd64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "b84607c7b8cccb68ee7453c37c8eeded5c395bb1f02626b84f9e42f09a256b44"
     end
     on_arm do
       url "https://github.com/GrantBirki/rust-template/releases/download/v#{version}/rust-template_v#{version}_linux-arm64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "d272ede47a3f4bd1931ed92579ca591af4bf9baa19ccac2d3b4c23339f84044c"
     end
   end
 
