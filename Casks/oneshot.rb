17f5e94928c7301b7326dbf6f4c0e6f58a5e5407
GrantBirki <grant.birkinbine@gmail.com>
2026-04-24 22:03:24 -0700

    Update OneShot cask: bump version to 1.1.0 and update SHA256 checksum

--- Casks/oneshot.rb
+++ Casks/oneshot.rb
@@ -1,6 +1,6 @@
 cask "oneshot" do
-  version "1.0.0"
-  sha256 "77f1cf3bdd044a624f603eda4ea61e01e7fb7f7722ff50c36f8b98ad884c4a49"
+  version "1.1.0"
+  sha256 "c5f0baba9f68fb382c0f1e4a19b09eb9b1b116f4fd98efc834556d161d94bf84"
 
   url "https://github.com/grantbirki/oneshot/releases/download/v#{version}/OneShot.zip"
   name "OneShot"
