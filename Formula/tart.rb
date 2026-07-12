c3edf23d4da03a8dd7e825a4be5d9674bda6502f
GrantBirki <grant.birkinbine@gmail.com>
2026-07-12 15:58:59 -0700

    Fix Tart formula style

--- Formula/tart.rb
+++ Formula/tart.rb
@@ -3,13 +3,12 @@
 class Tart < Formula
   desc "Run macOS and Linux VMs on Apple hardware"
   homepage "https://github.com/openai/tart"
+  url "https://github.com/openai/tart/releases/download/2.32.1/tart.tar.gz"
+  sha256 "8554ab4f7fc12afe52f9b7e3093a935673cbac737a83973d2db7a0683c814529"
   version "2.32.1"
   license "Fair Source"
 
-  url "https://github.com/openai/tart/releases/download/2.32.1/tart.tar.gz"
-  sha256 "8554ab4f7fc12afe52f9b7e3093a935673cbac737a83973d2db7a0683c814529"
-
-  depends_on :macos => :ventura
+  depends_on macos: :ventura
 
   def install
     libexec.install Dir["*"]
