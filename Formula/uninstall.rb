453a297bd470a1282a77c60029c5a2b73a902359
GrantBirki <grant.birkinbine@gmail.com>
2026-07-16 11:16:56 -0700

    Update uninstall formula to 1.3.0

--- Formula/uninstall.rb
+++ Formula/uninstall.rb
@@ -1,9 +1,9 @@
 class Uninstall < Formula
   desc "CLI tool for macOS to uninstall an app from your system"
   homepage "https://github.com/GrantBirki/uninstall"
-  url "https://github.com/GrantBirki/uninstall/releases/download/v1.2.0/uninstall-1.2.0.tar.gz"
-  version "1.2.0"
-  sha256 "ab9f58dfdac6d5d3413a260c1b59c17f5fca63e3b9ec4535abf28ae73bc6941f"
+  url "https://github.com/GrantBirki/uninstall/releases/download/v1.3.0/uninstall-1.3.0.tar.gz"
+  version "1.3.0"
+  sha256 "11d43cbd698d21972f67e99f7ecf20e0b56e20204de2846a41520a836f542046"
   license "MIT"
 
   livecheck do
